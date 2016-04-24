8aaa6ad8e8f3e6c91bdea74753fff876bbdf0cad
procek93 <procek@wisc.edu>
2016-04-23 20:06:08 -0500

    -Fixed the points generator. Previous iteration did not take into account negative slopes correctly. New version now successfully generates points for all positive slope lines and all negative slope lines. Fixing the actual rasterizer generator now.

--- rasterizer/delta_select.v
+++
@@ -1,8 +0,0 @@
-//module checks the distances between testpoints and real point
-//outputs which delt is closer to the real point
-module delta_select(x_0, x_1, y_0, y_1, dy, );
-
-
-
-
-endmodule
---
+++ rasterizer/steep_calculator_TB.sv
@@ -0,0 +1,4 @@
+module steep_calculator_TB ();
+
+steep_calculator steep_calc(line_cap_reg, dy, dx, slope_steep);
+endmodule
