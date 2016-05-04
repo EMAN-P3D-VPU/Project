679030a6bce8399a02ba70cf699f6feb33d034dc
Kenneth Siu <kksiu@wisc.edu>
2016-05-03 21:56:28 -0500

    Fix spart and made line generator not need a background

--- rasterizer/Rasterizer_Top_Level.v
+++ rasterizer/Rasterizer_Top_Level.v
@@ -86,7 +86,7 @@ LINE_GENERATOR LINE_GENERATOR(/*global inputs*/      .clk(clk), .rst(rst),
 				  /*input from object*/  .obj_change(obj_change),
 				  /*input from matrix*/  .bk_color(bk_color),
 				  /*input from f_buff*/  .frame_ready(frame_ready),
-				  /*output to f_buff*/   .raster_done(raster_done), .frame_rd_en(frame_rd_en), .frame_x(x_generated), .frame_y(y_generated), .px_color(px_color), .octant(octant_out), .clr_color(clr_color));
+				  /*output to f_buff*/   .raster_done(raster_done), .frame_rd_en(frame_rd_en), .frame_x(x_generated), .frame_y(y_generated), .c_valid(valid), .px_color(px_color), .octant(octant_out), .clr_color(clr_color));
 
 point_swapback swap_back(.x_gen(x_generated), .y_gen(y_generated), .octant(octant_out), .clr_color(clr_color), .x_f(frame_x), .y_f(frame_y));
 //pipeline stage
